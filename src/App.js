17d3867f759ac1bf8933472f70d4334175ee9d1b
Shuayb Badoolah <shuayb.b99@gmail.com>
2020-04-27 19:17:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          page for testing v1 of portfolio
         </p>
         <a
           className="App-link"
