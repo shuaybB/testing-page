73ca31787a2735c32e12d573682ee9bb3c0d2875
Shuayb Badoolah <shuayb.b99@gmail.com>
2020-04-27 19:18:04 -0400

    Updates

---
+++ precache-manifest.0124e67336f037d1309c937a02359814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f19254cc5b31b05c14c3968aa98691",
+    "url": "/github.io/testing-page/index.html"
+  },
+  {
+    "revision": "543f80dd8e8bd05bbe78",
+    "url": "/github.io/testing-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f342b871be6ddcaab917",
+    "url": "/github.io/testing-page/static/js/2.3326f146.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/testing-page/static/js/2.3326f146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543f80dd8e8bd05bbe78",
+    "url": "/github.io/testing-page/static/js/main.5dbb3478.chunk.js"
+  },
+  {
+    "revision": "adbff49ed82837104368",
+    "url": "/github.io/testing-page/static/js/runtime-main.205d7173.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/testing-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
