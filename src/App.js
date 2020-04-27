216c97ab3fd3e3598f12a517ec181043e4e1a663
Shuayb Badoolah <shuayb.b99@gmail.com>
2020-04-27 19:36:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          page for testing v1 of portfolio
+          testing testing 123
         </p>
         <a
           className="App-link"
